6ab6e057e6491ffabb2989e0f6f62207376ccd9a
Rafael Quintas <202300310@estudantes.ips.pt>
2024-04-03 09:06:49 +0100

    Nível 2

--- AirTransportation.java
+++ AirTransportation.java
@@ -2,4 +2,25 @@ public class AirTransportation extends Transport {
     private String name;
     private int numberOfContainers;
     private static final double honorary = 0.04;
+    
+    public AirTransportation (String name, int numberOfContainers) {
+        this.name = name;
+        this.numberOfContainers = numberOfContainers;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getNumberOfContainers() {
+        return this.numberOfContainers;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setNumberOfContainers(int numberOfContainers) {
+        this.numberOfContainers = numberOfContainers;
+    }
 }
--- GroundTransportation.java
+++ GroundTransportation.java
@@ -1,4 +1,16 @@
 public class GroundTransportation extends Transport {
     private String licensePlate;
     private static final double honorary = 0.03;
+    
+    public GroundTransportation (String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+    
+    public String getLicensePlate() {
+        return this.licensePlate;
+    }
+    
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
 }
