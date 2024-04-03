84b5a68e4e9687afdb4504d805bd5b7066ff7757
Rafael Quintas <202300310@estudantes.ips.pt>
2024-04-03 09:54:42 +0100

    Nível 3
    Realizamos o nível 3 e corrigimos alguns pontos dos níveis anteriores (construtor, variável honorary etc..)

--- AirTransportation.java
+++ AirTransportation.java
@@ -1,11 +1,12 @@
 public class AirTransportation extends Transport {
     private String name;
     private int numberOfContainers;
-    private static final double honorary = 0.04;
+    private static final double honorary = 4;
     
     public AirTransportation (String name, int numberOfContainers) {
         this.name = name;
         this.numberOfContainers = numberOfContainers;
+        setFees(honorary);
     }
     
     public String getName() {
@@ -21,6 +22,27 @@ public class AirTransportation extends Transport {
     }
     
     public void setNumberOfContainers(int numberOfContainers) {
-        this.numberOfContainers = numberOfContainers;
+        if (numberOfContainers >= 0) {
+            this.numberOfContainers = numberOfContainers;
+        }
+    }
+    
+    @Override
+    public double getPriceWithFees() {
+        return getPrice() * (1.0 + (getFees() * 0.01));
+    }
+    
+    @Override
+    public String getTransportType() {
+        return "Transporte Aereo";
+    }
+    
+    @Override
+    public String toString(){
+        final StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+        sb.append(String.format("%15s: %s\n", "Nome", this.name));
+        sb.append(String.format("%15s: %s\n", "NºContentores", this.numberOfContainers));
+        return sb.toString();
     }
 }
--- GroundTransportation.java
+++ GroundTransportation.java
@@ -1,9 +1,10 @@
 public class GroundTransportation extends Transport {
     private String licensePlate;
-    private static final double honorary = 0.03;
+    private static final double honorary = 3;
     
     public GroundTransportation (String licensePlate) {
         this.licensePlate = licensePlate;
+        setFees(honorary);
     }
     
     public String getLicensePlate() {
@@ -12,5 +13,23 @@ public class GroundTransportation extends Transport {
     
     public void setLicensePlate(String licensePlate) {
         this.licensePlate = licensePlate;
+    }
+    
+    @Override    
+    public double getPriceWithFees() {
+        return getPrice() * (1.0 + (getFees() * 0.01));
+    }
+    
+    @Override
+    public String getTransportType() {
+        return "Transporte Terrestre";
+    }
+    
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+        sb.append(String.format("%15s: %s\n", "Matricula", this.licensePlate));
+        return sb.toString();
     }
 }
